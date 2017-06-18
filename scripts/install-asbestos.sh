984e951b2c368ea7eadcb7afbc1c18ed13cf5188
Gonçalo Baltazar <me@goncalomb.com>
2017-06-18 22:56:32 +0100

    Add installation from GitHub

--- scripts/install-asbestos.sh
+++ scripts/install-asbestos.sh
@@ -7,10 +7,20 @@ set -e
 DIR=$(dirname -- "$0")
 TMP_DIR=$(mktemp -dt AsbestosPHP.XXXXX)
 
-echo -e "\e[1mCloning AsbestosPHP...\e[0m"
+function exit_trap {
+	echo -e "\e[1mCleaning...\e[0m"
+	rm -rf "$TMP_DIR"
+}
+trap exit_trap EXIT
 
 cd "$DIR"
-git clone "../AsbestosPHP" "$TMP_DIR"
+(
+	echo -e "\e[1mTrying to clone AsbestosPHP locally...\e[0m"
+	git clone "../AsbestosPHP" "$TMP_DIR"
+) || (
+	echo -e "\e[1mCloning AsbestosPHP from GitHub (goncalomb/AsbestosPHP)...\e[0m"
+	git clone "https://github.com/goncalomb/AsbestosPHP.git" "$TMP_DIR"
+)
 git -C "$TMP_DIR" checkout -q "$ASBESTOS_VERSION"
 
 # set modified dates from the commit dates
@@ -27,7 +37,3 @@ echo -e "\e[1mCopying asbestos directory...\e[0m"
 cd "$INSTALL_PATH"
 rm -rf asbestos
 cp -rp "$TMP_DIR/asbestos" .
-
-echo -e "\e[1mCleaning...\e[0m"
-
-rm -rf "$TMP_DIR"
