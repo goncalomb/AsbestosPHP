d6c5e3c77e800eee904042c13108369221e84245
Gonçalo Baltazar <me@goncalomb.com>
2017-06-18 16:16:34 +0100

    Move example files to a www directory

--- asbestos/core.php
+++ asbestos/core.php
@@ -4,10 +4,21 @@ define('ASBESTOS_MICROTIME', (isset($_SERVER['REQUEST_TIME_FLOAT']) ? (float) $_
 define('ASBESTOS_TIME', floor(ASBESTOS_MICROTIME));
 
 define('ASBESTOS_VERSION', '0.0.0');
-
 define('ASBESTOS_DIR', __DIR__);
-define('ASBESTOS_ROOT_DIR', dirname(__DIR__));
-define('ASBESTOS_CLASSES_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . 'classes'));
+
+$path = get_included_files()[0];
+do {
+	$path_last = $path;
+	$path = dirname($path);
+	if ($path == $path_last) {
+		trigger_error('AsbestosPHP: unable to find root directory', E_USER_ERROR);
+		exit();
+	}
+} while (realpath($path . DIRECTORY_SEPARATOR . 'asbestos') != ASBESTOS_DIR);
+define('ASBESTOS_ROOT_DIR', $path);
+unset($path, $path_last);
+
+define('ASBESTOS_CLASSES_DIR', ASBESTOS_DIR . DIRECTORY_SEPARATOR . 'classes');
 define('ASBESTOS_THEME_DIR', ASBESTOS_ROOT_DIR . DIRECTORY_SEPARATOR . 'theme');
 
 spl_autoload_register(function($name) {
